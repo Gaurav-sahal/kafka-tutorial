1dec2ba420efda1407a19dc7828c88fd0ee8be89
Gaurav Sahal <gaurav.sahal10@gmail.com>
2024-01-18 23:30:42 +0530

    Add ConsumerDemo

---
+++ kafka-beginners-course/kafka-basics/src/main/java/org/example/kafka/ConsumerDemo.java
@@ -0,0 +1,64 @@
+package org.example.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Consumer!");
+
+        String groupId = "my-java-application";
+        String topic = "demo_java";
+
+        // create Producer Properties
+        Properties properties = new Properties();
+
+        // connect to localhost
+//        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // connect to Upstash playground
+        properties.setProperty("bootstrap.servers", "in-hen-11985-eu2-kafka.upstash.io:9092");
+        properties.setProperty("sasl.mechanism", "SCRAM-SHA-256");
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"aW4taGVuLTExOTg1JLCEA5S0WKHUG-RPC9XwyywxyZdPn2JbXU8SsgDWwMboMyc\" password=\"MTIzMzYyMWQtOTFlYi00YzYyLWI4NWItMTc5NmM4MjljY2Ex\";");
+
+        // create consumer configs
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("group.id", groupId);
+        properties.setProperty("auto.offset.reset", "earliest");    // Options: none/earliest/latest
+
+        // create a consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // subscribe to a topic
+        consumer.subscribe(Arrays.asList(topic));
+
+        // poll for records
+        while(true) {
+            log.info("Polling");
+
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+
+            for(ConsumerRecord<String, String> record : records) {
+                log.info("Key: " + record.key() + ", Value: " + record.value());
+                log.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+            }
+        }
+    }
+}
