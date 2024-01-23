bc7c9102ea13b2fd580d1d96b7a8a34543690e89
Gaurav Sahal <gaurav.sahal10@gmail.com>
2024-01-23 18:38:29 +0530

    Add WikimediaChangesProducer

---
+++ kafka-beginners-course/kafka-producer-wikimedia/src/main/java/org/example/kafka/wikimedia/WikimediaChangesProducer.java
@@ -0,0 +1,46 @@
+package org.example.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.background.BackgroundEventHandler;
+import com.launchdarkly.eventsource.background.BackgroundEventSource;
+import org.apache.kafka.clients.consumer.internals.events.EventHandler;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class WikimediaChangesProducer {
+    public static void main(String[] args) throws InterruptedException {
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "in-hen-11985-eu2-kafka.upstash.io:9092");
+        properties.setProperty("sasl.mechanism", "SCRAM-SHA-256");
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"aW4taGVuLTExOTg1JLCEA5S0WKHUG-RPC9XwyywxyZdPn2JbXU8SsgDWwMboMyc\" password=\"MTIzMzYyMWQtOTFlYi00YzYyLWI4NWItMTc5NmM4MjljY2Ex\";");
+
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "wikimedia.recentchange";
+
+        BackgroundEventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder eventSourceBuilder = new EventSource.Builder(URI.create(url));
+        BackgroundEventSource.Builder builder = new BackgroundEventSource.Builder(eventHandler, eventSourceBuilder);
+        BackgroundEventSource eventSource = builder.build();
+
+        // start the producer in another thread
+        eventSource.start();
+
+        // we produce for 10 minutes and block the program until then
+        TimeUnit.MINUTES.sleep(10);
+    }
+}
